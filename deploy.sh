bc0e40cffdb680c6304e5839bc232d71b0d237b1
Louis Carvalho <louis.carvalho98@gmail.com>
2022-12-12 18:01:11 +0100

    correction deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/LouisC98/CV_VueJS.git master:gh-pages
+git push -f https://github.com/LouisC98/CV.git master:gh-pages
 
 cd -
