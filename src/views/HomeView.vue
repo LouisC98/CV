0ee0c8e223b3caea0a521bd906d870945285df1e
Louis Carvalho <louis.carvalho98@gmail.com>
2022-12-15 13:32:33 +0100

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,26 @@
 <script>
+import axios from 'axios';
+
 export default {
   methods: {
-    openPdfInNewTab() {
-      // URL du document PDF que vous voulez ouvrir
-      const pdfUrl = '/CV_CARVALHO_LOUIS.pdf';
-      
-      // Ouvre un nouvel onglet dans le navigateur
-      window.open(pdfUrl, '_blank');
+    async downloadPdf() {
+      fetch('CV_LOUIS_CARVALHO.pdf')
+        .then(response => response.blob())
+        .then(blob => {
+          
+          const url = URL.createObjectURL(blob);
+
+          const link = document.createElement('a');
+          link.href = url;
+          link.download = 'CV_LOUIS_CARVALHO.pdf';
+          document.body.appendChild(link);
+          link.click();
+
+          link.parentNode.removeChild(link);
+          setTimeout(() => URL.revokeObjectURL(url), 100);
+        });
     }
-}
+  }
 }
 </script>
 <template>
@@ -19,7 +31,7 @@ export default {
       <h2 class="fullstack">Fullstack</h2>
     </div>
     <div class="download-btn mt-5">
-      <button @click="openPdfInNewTab">Télécharger CV<i class="fa-solid fa-download"></i></button>
+      <button @click="downloadPdf('./CV_LOUIS_CARVALHO.pdf')">Télécharger CV<i class="fa-solid fa-download"></i></button>
     </div>
     <div class="socials d-flex justify-content-center my-5">
         <a href="https://www.linkedin.com/in/louis-carvalho/" class="linkedin">
