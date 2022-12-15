816bd3bb9e008b4fa058aec9ccb51cce7503d1f6
Louis Carvalho <louis.carvalho98@gmail.com>
2022-12-15 13:17:00 +0100

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <script>
 export default {
   methods: {
-  downloadPdf() {
-    const pdfUrl = '/CV_LOUIS_CARVALHO.pdf';
-
-    const a = document.createElement('a');
-
-    a.href = pdfUrl;
-
-    a.download = 'CV_LOUIS_CARVALHO.pdf';
-
-    a.click();
-  },
-},
+    openPdfInNewTab() {
+      // URL du document PDF que vous voulez ouvrir
+      const pdfUrl = '/CV_CARVALHO_LOUIS.pdf';
+      
+      // Ouvre un nouvel onglet dans le navigateur
+      window.open(pdfUrl, '_blank');
+    }
+}
 }
 </script>
 <template>
@@ -23,7 +19,7 @@ export default {
       <h2 class="fullstack">Fullstack</h2>
     </div>
     <div class="download-btn mt-5">
-      <button @click="downloadPdf">Télécharger CV<i class="fa-solid fa-download"></i></button>
+      <button @click="openPdfInNewTab">Télécharger CV<i class="fa-solid fa-download"></i></button>
     </div>
     <div class="socials d-flex justify-content-center my-5">
         <a href="https://www.linkedin.com/in/louis-carvalho/" class="linkedin">
